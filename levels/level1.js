2636289421339317fadc38eeac62788ca3514480
Bastian Weschasit <bastian.weschasit@iridium-works.com>
2024-05-09 21:29:51 +0200

    .

--- levels/level1.js
+++ levels/level1.js
@@ -1,11 +1,11 @@
 const level1 = new Level(
     [
         new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new SmallChicken(),
-        new SmallChicken(),
-        new SmallChicken(),
+        // new Chicken(),
+        // new Chicken(),
+        // new SmallChicken(),
+        // new SmallChicken(),
+        // new SmallChicken(),
         new Endboss()
     ],
     [
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -10,17 +10,19 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
 
+    IMAGES_DEAD = [
+        'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+    ];
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
-
+        this.loadImages(this.IMAGES_DEAD);
         this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700
         this.speed = 0.15 + Math.random() * 0.5;
 
         this.animate();
     }
-
-
 
     animate() {
         setInterval(() => {
@@ -30,6 +32,8 @@ class Chicken extends MovableObject {
         setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
         }, 200);
+
+
     }
 
 
