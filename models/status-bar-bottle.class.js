c5df0ea82fd97ffd3fcfa974eb257f42e81d73bc
Bastian Weschasit <bastian.weschasit@iridium-works.com>
2024-05-13 16:58:29 +0200

    fixed statusbar coin

--- models/status-bar-bottle.class.js
+++ models/status-bar-bottle.class.js
@@ -9,7 +9,7 @@ class StatusBarBottle extends DrawableObject {
     'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png',
   ];
 
-  percentage = 100;
+  percentage = 0;
 
   constructor() {
     super();
@@ -18,7 +18,7 @@ class StatusBarBottle extends DrawableObject {
     this.y = 0;
     this.width = 200;
     this.height = 60;
-    this.setPercentage(100);
+    this.setPercentage(this.percentage);
   }
 
   // setPercentage(50);
--- models/status-bar-coin.class.js
+++ models/status-bar-coin.class.js
@@ -10,7 +10,7 @@ class StatusBarCoin extends DrawableObject {
     'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png',
   ];
 
-  percentage = 100;
+  percentage = 0;
 
 
   constructor() {
@@ -20,7 +20,7 @@ class StatusBarCoin extends DrawableObject {
     this.y = 80;
     this.width = 200;
     this.height = 60;
-    this.setPercentage(100);
+    this.setPercentage(this.percentage);
   }
 
   // setPercentage(50);
