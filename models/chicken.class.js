533908771fd80f46107eed0936c68fde08821575
Bastian Weschasit <bastian.weschasit@iridium-works.com>
2024-05-19 14:25:24 +0200

    .

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,11 +1,9 @@
-let chickenPositions = []; // Array zum Speichern der X-Positionen der Hühner
-const MIN_DISTANCE = 100; // Mindestabstand zwischen den Hühnern
-
 class Chicken extends MovableObject {
 
+
     height = 55;
-    width = 70;
-    y = 360;
+    width = 70
+    y = 360
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
@@ -20,22 +18,10 @@ class Chicken extends MovableObject {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_DEAD);
-        this.x = this.generatePosition(); // Generiere eine Position mit Abstand
+        this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700
         this.speed = 0.15 + Math.random() * 0.5;
+
         this.animate();
-    }
-
-    generatePosition() {
-        let position;
-        let validPosition = false;
-
-        while (!validPosition) {
-            position = 200 + Math.random() * 500; // Zahl zwischen 200 und 700
-            validPosition = chickenPositions.every(pos => Math.abs(pos - position) >= MIN_DISTANCE);
-        }
-
-        chickenPositions.push(position);
-        return position;
     }
 
     animate() {
@@ -52,7 +38,7 @@ class Chicken extends MovableObject {
         }, 200);
     }
 
-}
 
+}
 
 
